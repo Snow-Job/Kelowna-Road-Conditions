97bd25f43872f4a1ed70e62a9abd2e45f0a7abdd
yynickel <yynickel@gmail.com>
2016-02-26 14:35:57 -0800

    --put the map function into a separate js file

--- index.php
+++ index.php
@@ -27,81 +27,8 @@
 
 
     <!-- google maps -->
-    <script>
-			var plowRoute=[];
-			var map;
-      function initMap() {
-        var mapDiv = document.getElementById('map');
-        map = new google.maps.Map(mapDiv, {
-          center: {lat: 49.8801, lng: -119.4436},
-          zoom: 12
-        });
+    <script src="js/mapFunc.js"></script>
 
-
-        //query csv file named "text.csv" and process it
-            $.ajax({
-                type:"GET",
-                url:"data/test.csv",
-                dataType:"text",
-                success:function(data){
-                    processData(data);}
-            });
-        }
-
-        function processData(data){
-            //get the latitude and longitude
-            var allDataLines=data.split(/\r\n|\n/);
-            var headers=allDataLines[8].split(','); // in this case, headers are at 9th line in the csv
-            for(var i=9;i<19;i++){                  // get data from line 10 to line 19
-                var line=parseRow(allDataLines[i]);
-                plowRoute.push(line[7]+','+line[6]);// latitude is in 8th cell, longitude is in 7th cell
-            }
-            //get snap-to-road points according to the points provide and plot
-            $.get('https://roads.googleapis.com/v1/snapToRoads', {
-                interpolate: true,
-                key:'AIzaSyBaWC_s7uAHTX6BeKramcTZDaFfXTh5k74',
-                path: plowRoute.join('|')
-            },function(data){
-                var snappedCoordinates=[];
-                for(var i=0;i<data.snappedPoints.length;i++){
-                    var latlng = new google.maps.LatLng(
-                        data.snappedPoints[i].location.latitude,
-                        data.snappedPoints[i].location.longitude);
-                    snappedCoordinates.push(latlng);
-                }
-                var plowPath=new google.maps.Polyline({
-                    path:snappedCoordinates,
-                    strokeColor:'#FF0000',
-                    strokeOpacity:1.0,
-                    strokeWeight:2
-                });
-                plowPath.setMap(map);
-            });
-        }
-        //parse and split data by comma, and take care of commas in quotes/
-        function parseRow(str){
-            var insideQuote = false,
-            entries = [],
-            entry = [];
-            str.split('').forEach(function (character) {
-                if(character === '"') {
-                    insideQuote = !insideQuote;
-                } else {
-                    if(character == "," && !insideQuote) {
-                        entries.push(entry.join(''));
-                        entry = [];
-                    } else {
-                        entry.push(character);
-                    }
-                }
-            });
-            entries.push(entry.join(''));
-            return entries;
-        }
-
-    </script>
-    <script src="https://maps.googleapis.com/maps/api/js?callback=initMap"
-        async defer></script>
 
     <!-- resizes header when you scroll down -->
     <script>
