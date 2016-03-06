7e47e44216761153487e727c8f3dd59aacc2e83f
C. James B. Rogers <cjbrogers@gmail.com>
2016-03-06 12:17:47 -0800

    Implemented an MVC structure for our code

---
+++ public/js/headerScroll.js
@@ -0,0 +1,23 @@
+function init() {
+    window.addEventListener('scroll', function(e){
+
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 100,
+            header = document.querySelector("header");
+        if (distanceY > shrinkOn) {
+            classie.add(header,"smaller");
+            var image = document.getElementById('imgLogo');
+            image.src = 'images/logoSmall.png';
+            image.style.width = '150px';
+            image.style.padding = '5px 0 0 0';
+        } else {
+            if (classie.has(header,"smaller")) {
+            classie.remove(header,"smaller");
+            var image = document.getElementById('imgLogo');
+            image.src = 'images/logo.png';
+            image.style.width = '150px';
+            image.style.padding = '20px 0 0 0';
+            }
+        }
+    });
+}
