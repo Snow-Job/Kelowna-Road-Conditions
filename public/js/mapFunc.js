fc46863059e609e31f6cfa5ad8ff859a6afa7ad7
yynickel <yynickel@gmail.com>
2016-03-09 15:26:28 -0800

    working but needs improvement

--- public/js/mapFunc.js
+++ public/js/mapFunc.js
@@ -1,6 +1,6 @@
 var plowRoute=[];
 var truckRoute=[];
-var color=["#FF6600","#FF3300","#FF0000","#CC3300","#FF0033","#FF0066","#CC0099","#9900CC","#6600FF","#3300FF","#0033CC","#3366FF","#0099FF","#33CCFF","#00CC99","#00FF66","#FFCC33"];
+var color=["#FF6600","#FF3300","#FF0000","#CC3300","#CC0033","#FF3366","#FF0033","#FF0066","#CC0099","#9900CC","#6600FF","#3300FF","#0033CC","#3366FF","#0099FF","#33CCFF","#00CC99","#00FF66","#FFCC33","#FF9900","#FFCC00","#CC9900","#99CC00","CCFF00"];
 function initMap() {
   var mapDiv = document.getElementById('map');
   map = new google.maps.Map(mapDiv, {
@@ -14,6 +14,7 @@ function initMap() {
           type:"GET",
           url:"../application/views/dataUpdate.php",
           success:function(data){
+            console.log(data);
             processData(data);}
       });
   }
@@ -25,7 +26,7 @@ function initMap() {
           var id=temp[0];
       for(var i=0;i<allDataLines.length;i++){                  // get data from line 10 to line 19
           var line=parseRow(allDataLines[i]);
-          if (id!=line[0]){
+          if (id!==line[0]){
             id=line[0];
             plowRoute.push(truckRoute);
             truckRoute=[];
@@ -34,30 +35,38 @@ function initMap() {
       }
       //get snap-to-road points according to the points provide and plot
       for(var ii=0;ii<plowRoute.length;ii++){
-      $.get('https://roads.googleapis.com/v1/snapToRoads', {
-          interpolate: true,
-          key:'AIzaSyBaWC_s7uAHTX6BeKramcTZDaFfXTh5k74',
-          path: plowRoute[ii].join('|')
-      },function(data){
-          var snappedCoordinates=[];
-          for(var i=0;i<data.snappedPoints.length;i++){
-              var latlng = new google.maps.LatLng(
+        var start=0,end=100;
+        var segment=[];
+        while (start<plowRoute[ii].length){
+          if (end>plowRoute[ii].length) {end=plowRoute[ii].length;}
+          segment=plowRoute[ii].slice(start,end);
+          start+=99;
+          end=start+100;
+          if (segment.length>0) {
+            $.get('https://roads.googleapis.com/v1/snapToRoads', {
+              interpolate: true,
+              key:'AIzaSyC-qqLqVzYXPH3qTDrguSFVTV07Bk59lNI',
+              path: segment.join('|')
+            },function(data){
+              var snappedCoordinates=[];
+              for(var i=0;i<data.snappedPoints.length;i++){
+                var latlng = new google.maps.LatLng(
                   data.snappedPoints[i].location.latitude,
                   data.snappedPoints[i].location.longitude);
-              snappedCoordinates.push(latlng);
+                  snappedCoordinates.push(latlng);
+                }
+              var plowPath=new google.maps.Polyline({
+                path:snappedCoordinates,
+                strokeColor:color.pop(),
+                strokeOpacity:1.0,
+                strokeWeight:4
+              });
+              plowPath.setMap(map);
+            });
           }
-
-          var plowPath=new google.maps.Polyline({
-              path:snappedCoordinates,
-              strokeColor:color.pop(),
-              strokeOpacity:1.0,
-              strokeWeight:4
-          });
-          plowPath.setMap(map);
-      });
+        }
+      }
     }
-
-  }
 
   //parse and split data by comma, and take care of commas in quotes/
   function parseRow(str){
