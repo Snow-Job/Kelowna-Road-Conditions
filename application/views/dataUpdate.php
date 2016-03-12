dfadf88ebb9dd5423ebf745ec3ba50cb659dbb6d
C. James B. Rogers <cjbrogers@gmail.com>
2016-03-12 13:04:39 -0800

    Changed day for better data

--- application/views/dataUpdate.php
+++ application/views/dataUpdate.php
@@ -5,7 +5,7 @@ $mysqli = mysqli_connect('cosc304.ok.ubc.ca', 'mjoseph', '15057136', 'db_mjoseph
 
 #get range of times to query on db based on current time of day then format
 date_default_timezone_set('PST8PDT');
-$tsNow = mktime(date('H'), date('i'), date('s'), 12, 31, 2015);
+$tsNow = mktime(date('H'), date('i'), date('s'), 12, 27, 2015);
 $nowTime = date('Y-m-d H:i:s', $tsNow - (60*10));
 $futureTime = date('Y-m-d H:i:s', $tsNow);
 #create array to store points in and set of query
